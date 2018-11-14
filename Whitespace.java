0cdbcd0b9e129cce81cd28d52a3f2230e3661e5d
Lane W. Surface <surfacelane@gmail.com>
2018-11-13 21:32:38 -0600

    F%#GE!

---
+++ Whitespace.java
@@ -0,0 +1,36 @@
+                                                                                /*
+By dark we toll in the depths                                                   ,
+and in day we dare not show ourselves                                           ,
+for we are a legion of desparates                                               ,
+and the world cast us aside                                                     .
+Now is our time                                                                 ,
+my brethren                                                                     .
+Bear the cross of the mighty one                                                ,
+and be not afraid of condemnation                                               .
+Those who mocked our ways have risen                                            ;
+it is our time to inhabit the desolate land                                     !
+                                                                                */
+class Whitespace                                                                {   
+                static int f                                                    (
+                            int   n                                             ,
+                            float r                                             ){
+                    return                                                      (
+                          int                                                   )(
+                             n                                                  * 
+                             r                                                  );}
+                public static void main                                         (
+                                       String                                   []
+                                             args                               ){
+                    System                                                      .
+                    out                                                         .
+                    println                                                     (
+                           "Hello,  O  beautiful  world!"                       );
+                    int v                                                       =
+                         f                                                      (
+                          4                                                     ,
+                          3.14f                                                 );
+                    System                                                      .
+                    out                                                         .
+                    print                                                       (
+                         v                                                      );
+                                                                                }}
